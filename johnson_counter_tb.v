276c3ec4f44db8d84aebf0eca4a70bcf4a28fbb5
deepakkumar-04 <rdeepakkumar2022@gmail.com>
2024-12-31 14:50:45 +0530

    Add files via upload

---
+++ johnson_counter_tb.v
@@ -0,0 +1,16 @@
+module johnson_counter_tb;
+reg clk, rst;
+wire [4:0] count;
+johnson_counter_rtl uut (
+    .clk(clk),
+    .rst(rst),
+    .count(count));
+initial begin
+    clk = 0;
+    rst = 1;
+    #10 
+    rst = 0;
+end
+always #5 clk = ~clk;
+endmodule
+
